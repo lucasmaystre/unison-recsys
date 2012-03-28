f73bd10505012ca601a551ada289f43041990451
Lucas Maystre <lucas@maystre.ch>
2012-03-28 13:43:57 +0200

    Compute similarity from JSON files.
    This diff changes the semantics of 'similarity.py'. Until now, the tracks were
    specified using a minimalist pseudo-language directly on the command line. From
    now on, it compares tracks based on a JSON file containig their metadata.
    
    The file should have the format of the MSD's last.fm dataset. Basically:
    
        {'artist': ".", 'title': ".", 'tags': [['rock', 1.0], ['pop', 0.0]]}

--- similarity.py
+++ similarity.py
@@ -6,68 +6,60 @@ product of the normalized track vectors.
 """
 
 import argparse
-import math
+import json
 import sqlite3
 import struct
 
-QUERY_SELECT = "SELECT vector FROM tags WHERE name = ?"
+from math import log, log1p, sqrt
+from util import (DB_PATH, TAGS_PATH, WEIGHTS_PATH,
+        get_vector, print_vector, print_track, load_tags, load_weights)
 
 
-def similarity(t1, t2, db):
+def similarity(t1, t2, db, weight_fct):
     conn = sqlite3.connect(db)
-    tag = dict()
-    for name, val in (t1 + t2):
-        res = conn.execute(QUERY_SELECT, (name,)).fetchone()
-        if res is None:
-            raise Exception("Tag '%s' was not found in the database." % name)
-        tag[name] = list()
-        raw = res[0]
-        for i in xrange(0, len(raw), 4):
-            component, = struct.unpack('!f', raw[i:i+4])
-            tag[name].append(component)
-    v1 = compute_vector(t1, tag)
-    v2 = compute_vector(t2, tag)
+    v1 = compute_vector(t1, weight_fct, conn)
+    v2 = compute_vector(t2, weight_fct, conn)
     # Cosine similarity (= dot product, vectors are normalized).
     return sum([v1[i] * v2[i] for i in range(len(v1))])
 
 
-def compute_vector(track, tag):
-    # Figure out the dimensionality.
-    dim = len(tag.itervalues().next())
+def compute_vector(track, weight_fct, db_conn):
+    dim = len(get_vector(db_conn, 'rock'))  # TODO temp.
     vector = [0] * dim
-    # Add the weighted values for each tag.
-    for name, val in track:
-        vector = [(val*x + y) for x, y in zip(tag[name], vector)]
-    # Normalize.
-    norm = math.sqrt(sum([x*x for x in vector]))
-    return tuple([x / norm for x in vector])
-    
-
-def track_info(info):
-    # Treat it as Unicode, and discard the case.
-    decoded = unicode(info, encoding='utf-8').lower()
-    elems = decoded.split(',')
-    tags = list()
-    for elem in elems:
-        values = elem.split('=')
-        if len(values) == 2:
-            tags.append((values[0], float(values[1])))
-        else:
-            tags.append((values[0], 100.0))
-    return tuple(tags)
+    for tag, count in track['tags']:
+        curr = get_vector(db_conn, tag)
+        if curr is None:
+            # TODO Not very elegant.
+            print "Tag %s not in database." % tag
+            continue
+        weight = weight_fct(tag) * log1p(float(count)) / log(2)
+        vector = [(weight*x + y) for x, y in zip(curr, vector)]
+    norm = sqrt(sum([x*x for x in vector]))
+    if norm > 0:
+        return tuple([x / norm for x in vector])
+    else:
+        return tuple(vector)
 
 
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('t1', type=track_info)
-    parser.add_argument('t2', type=track_info)
-    parser.add_argument('--db', required=True)
+    parser.add_argument('track1', type=open)
+    parser.add_argument('track2', type=open)
+    parser.add_argument('--db', default=DB_PATH)
+    parser.add_argument('--weights', default=WEIGHTS_PATH)
+    parser.add_argument('--tags', default=TAGS_PATH)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = _parse_args()
-    try:
-        print similarity(args.t1, args.t2, args.db)
-    except Exception as e:
-        print e
+    t1 = json.loads(args.track1.read())
+    t2 = json.loads(args.track2.read())
+    print('-------- Track 1')
+    print_track(t1)
+    print('-------- Track 2')
+    print_track(t2)
+    weights = load_weights(args.weights)
+    tags = load_tags(args.tags)
+    weight_fct = lambda tag: weights[tags[tag]]
+    print similarity(t1, t2, args.db, weight_fct)
