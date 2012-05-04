880aea51b0430ce8be5a5a8822d9607155d8540e
Lucas Maystre <lucas@maystre.ch>
2012-05-04 16:37:29 +0200

    Set root URL via UNISON_API_ROOT.

--- scripts/api.py
+++ scripts/api.py
@@ -2,6 +2,7 @@
 import argparse
 import ast
 import base64
+import os
 import requests
 import requests.auth
 
@@ -9,20 +10,21 @@ import requests.auth
 DEFAULT_MAIL = 'a@gs.com'
 DEFAULT_PASSWORD = 'h3ll0'
 
+DEFAULT_API_ROOT = 'http://api.unison.local'
+
 
 class UnisonGetter:
 
-    URL_FORMAT = "http://api.unison.local%s"
-
-    def __init__(self, mail, password):
+    def __init__(self, root, mail, password):
         mail = base64.b64encode(mail)
         password = base64.b64encode(password)
         self.auth = requests.auth.HTTPBasicAuth(mail, password)
+        self.root = root
 
     def __getattr__(self, name):
         getter = getattr(requests, name)
         def wrapper(path, data=None):
-            url = self.URL_FORMAT % path
+            url = self.root + path
             res = getter(url, data=data, auth=self.auth)
             print "method: %s" % name.upper()
             print "URL:    %s" % url
@@ -45,7 +47,8 @@ def _parse_args():
 
 if __name__ == '__main__':
     args = _parse_args()
-    unison = UnisonGetter(args.mail, args.password)
+    api_root = os.environ.get('UNISON_API_ROOT', DEFAULT_API_ROOT)
+    unison = UnisonGetter(api_root, args.mail, args.password)
     fct = getattr(unison, args.method.lower())
     if args.data:
         raw = raw_input('data: ')
